4a529b713a63ca2c11555665a207e41c71e04d78
mcrokes <malfonso2108@gmail.com>
2024-06-27 19:57:07 -0400

    FET-8 fix: styling dataset table

--- Dashboard/dataset/datasetLayout.py
+++ Dashboard/dataset/datasetLayout.py
@@ -145,8 +145,8 @@ def datasetCallbacks(app, furl: Function):
                             data=df.to_dict("records"),
                             columns=[{"name": i, "id": i} for i in df.columns],
                             page_size=10,
-                        ),
-                    ]
+                        )
+                    ], className="rules-table"
                 ),
                 [
                     html.Div(
